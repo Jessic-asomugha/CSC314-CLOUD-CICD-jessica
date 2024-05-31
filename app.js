9578fe8299292e8cc7d374dff084b19871fa0104
jessic-asomugha <jessicaasomegha266@gmail.com>
2024-05-31 16:25:18 +0100

    addded a message containig name and mat no

--- app.js
+++ app.js
@@ -6,3 +6,4 @@ app.get('/', (req, res) => res.send(' this is cloud computing DEVOPS ; CI-CD PIP
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+console.log('THIS IS CSC314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS, THIS PAGE INDICATES SUCCESS \n Asomugha Jessica \n VUG/CSC/21/5540 ');
